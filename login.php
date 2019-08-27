93d042484789c1c27b4c7611480b053ea8060309
Charry300 <47616444+Charry300@users.noreply.github.com>
2019-08-27 11:05:39 -0500

    linea base
    creo un archivo de login y carpeta de css que es la decoracion de mi pagina web

---
+++ login.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Supermercado Los Alpes</title>
+  </head>
+      <body background="3.jpg">
+        <h1 style="text-align: center; margin : 50px 0 0 0; color: white">Los Alpes</h1>
+          <div class=" row">
+            <div class="col-12 col-xd-4 col-lg-4">
+            </div>
+          <div class="col-12 col-xd-4 col-lg-4">
+        <form action="registro.php" method="post">
+            <div class="form-group">
+              <label for="exampleInputEmail1"></label>
+              <input type="text" name="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Corre Electronico">
+              <small id="emailHelp" class="form-text text-muted"></small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1"></label>
+                <input type="password" name="" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1"></label>
+            </div>
+            <center>
+              <button type="submit" class="btn btn-success">Calcular</button>
+            </center>   
+        </form>
+        <center>
+        <a href="Registro.php"  style="margin: 400 0 0 611 ; color: white">Regristrarse</a>
+        <center>
+            </div>
+              <div class="col-12 col-xd-4 col-lg-4">
+            </div>
+            
+
+          
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>

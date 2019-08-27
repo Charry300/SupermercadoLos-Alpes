3d3ed3c966cb7aa552d7174af809317c55ae5179
Charry300 <47616444+Charry300@users.noreply.github.com>
2019-08-27 11:12:38 -0500

    base del proyecto
    agrego archivos de mi proyecto. registro y n ademas las imagenes que estoy utilizando un archivo de text

---
+++ n.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Supermercado Los Alpes</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/estilos.css">
+
+</head>
+<body>
+    <header> 
+    <div class="contenedor">
+        <h1 class="icon-cafe">Los Alpes</h1>
+        <input type="checkbox" id="menu-bar">
+        <label class="icon-menu" for="menu-bar"></label>
+        <nav class="menu">
+            <a href="">Mercado</a>
+            <a href="">Electrodomesticos</a>
+            <a href="">Hogar</a>
+            <a href="">Deportes y Tiempos Libres</a>
+        </nav>
+    </div>
+    </header>
+    <main>
+        <section id="banner">
+            <img src="1.jpeg">
+        <div class="contenedor">
+            <h2>Frutas Y Verduras</h2>
+            <p>¿Cual es su Frutas y Verduras Favoritas?</p>
+            <a href="">Leer Mas</a>
+        </div>
+        </section>
+            <section id="Bienvenidos">
+                <h2>BIENVENIDOS A NUESTRO SUPERMERCADO</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, cumque quibusdam! Reprehenderit esse, dignissimos doloremque omnis, repellat distinctio ut laudantium porro cum facilis incidunt facere nulla deserunt exercitationem. Eos, fugiat.  
+                </p>
+            </section>
+            <section id="blog">
+                <h3>Nuestros Producto</h3>
+                    <div class="contenedor">
+                    <article>
+                        <img src="22.jpg">
+                        <h4>Mercado</h4>
+                    </article>
+                    <article>
+                        <img src="11.jpg">
+                        <h4>Electrodomesticos</h4>
+                    </article>
+                    <article>
+                        <img src="33.jpg">
+                        <h4>Hogar</h4>
+                    </article>
+                    <article>
+                        <img src="44.jpg">
+                        <h4>Deportes y Tiempos Libres</h4>
+                    </article>
+                    </div>
+            </section>
+            <section id="info">
+                <h3>Conoce mas a fondo nuestros productos del mejor supermercado a nivel nacional</h3>
+                <div class="contenedor">
+                    <div class="info-pet">
+                        <img src="55.jpg" alt="">
+                        <h4>Mision</h4>
+                    </div>  
+                    <div class="info-pet">
+                        <img src="66.jpg" alt="">
+                        <h4>Vision</h4>
+                    </div>   
+                    <div class="info-pet">
+                        <img src="77.jpg" alt="">
+                        <h4>Informacion</h4>
+                    </div>      
+                </div>
+            </section>
+    </main>
+    <footer>
+        <div class="contenedor">
+            <p class="copy">Supermercado &copy; 2019</p>
+            <div class="sociales">
+                <a class="icon-facebook-1" href="#"></a>
+                <a class="icon-gplus" href="#"></a>
+                <a class="icon-twitter" href="#"></a>
+            </div>
+
+        </div>
+    </footer>
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shirnk-to-fit=no">
+     <!-- Bootstrap CSS -->
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registrar</title>
+    </head>
+        <body background="">
+            <h1 style="text-align: center; margin : 40px 0 0 0; color:black">Datos Personales</h1>
+                <form action="http://localhost/programacion1/login.php" method="post">
+                    <div>
+                        <label for="name">Nombre:</label>
+                        <input type="text" id="name" />
+                    </div>
+                    <div>
+                        <label for="surname">Apellido:</label>
+                        <input type="text" id="surname" />
+                    </div>
+                    <div>
+                        <label for="mail">Correo Electronico:</label>
+                        <input type="email" id="mail" />
+                    </div>
+                    <div>
+                        <label for="text">Contraseña:</label>
+                        <input type="password" id="text" />
+                    </div>
+                    <div>
+                        <label for="text">Repita Contraseña:</label>
+                        <input type="password" id="text" />
+                    </div>
+                    <div class="button">
+                        <button type="submit">Guardar</button>
+                    </div>
+                </form>
+        <style type="text/css">
+            form 
+            {
+             /* Sólo para centrar el formulario a la página */
+                margin: 0 auto;
+                width: 400px;
+                /* Para ver el borde del formulario */
+                padding: 1em;
+                border: 1px solid #CCC;
+                border-radius: 1em;
+            }   
+            form div + div 
+            {
+                margin-top: 1em;
+            }
+            label 
+            {
+                /* Para asegurarse que todos los labels tienen el mismo tamaño y están alineados correctamente */
+                display: inline-block;
+                width: 90px;
+                text-align: right;
+            }
+            input, textarea
+            {
+                /* Para asegurarse de que todos los campos de texto tienen las mismas propiedades de fuente
+                Por defecto, las areas de texto tienen una fuente con monospace */
+                font: 1em sans-serif;
+
+                /* Para darle el mismo tamaño a todos los campos de texto */
+                width: 300px;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+
+                /* Para armonizar el look&feel del borde en los campos de texto */
+                border: 1px solid #999;
+            }
+            input:focus, textarea:focus 
+            {
+                /* Para dar un pequeño destaque en elementos activos*/
+                border-color: #000;
+            }
+            textarea 
+            {
+                /* Para alinear campos de texto multilínea con sus labels */
+                vertical-align: top;
+
+                /* Para dar suficiente espacio para escribir texto */
+                height: 5em;
+
+                /* Para permitir a los usuarios cambiar el tamaño de cualquier textarea verticalmente
+                    No funciona en todos los navegadores */
+                resize: vertical;
+            }
+            button 
+            {
+                /* Para posicionar los botones a la misma posición que los campos de texto */
+                padding-left: 20px; /* mismo tamaño a todos los elementos label */
+            }
+            button 
+            {
+                /* Este margen extra representa aproximadamente el mismo espacio que el espacio
+                entre los labels y sus campos de texto */
+                margin-left: 13em;
+            }
+        </style>
+ 
+        </body>
+    </html>
